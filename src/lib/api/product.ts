73e80a9bc6e83e7c2e48422ecb6f8fc5698a4b8b
Greg <gregory.jacob@cine.digital>
2026-04-08 15:25:13 +0200

    Product query

--- src/lib/api/product.ts
+++ src/lib/api/product.ts
@@ -5,7 +5,7 @@ import { ProductResponse } from '@/types/api/Product';
 export const getShortProductList = async (): Promise<ProductResponse> => {
     try {
         const response = await fetch(
-            'https://dummyjson.com/productsZZZ/category/groceries?sortBy=title&order=asc',
+            'https://dummyjson.com/products/category/groceries?sortBy=title&order=asc',
         );
         const data: ProductResponse = await response.json();
 
